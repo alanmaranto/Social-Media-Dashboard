a05e4d39933414157ca7348b6749318b53b80bbd
alan <alanarostegui@hotmail.com>
2020-06-07 15:59:11 -0500

    refactor(Header): separate header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
+import Header from './components/header/Header';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <div className="wrapper">
-          <div className="header-grid">
-            <div>
-              <h1>Social Media Dashboard</h1>
-              <p className="header-total">Total Followers 23, 005</p>
-            </div>
-            <div className="dark-mode">
-              <p className="dark-mode title">Dark Mode</p>
-              <input type="checkbox" className="checkbox" id="checkbox" />
-              <label htmlFor="checkbox" className="switch"></label>
-            </div>
-          </div>
-        </div>
-      </header>
-    </div>
+    <Header />
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="wrapper">
+          <div className="header-grid">
+            <div>
+              <h1>Social Media Dashboard</h1>
+              <p className="header-total">Total Followers 23, 005</p>
+            </div>
+            <div className="dark-mode">
+              <p className="dark-mode title">Dark Mode</p>
+              <input type="checkbox" className="checkbox" id="checkbox" />
+              <label htmlFor="checkbox" className="switch"></label>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;

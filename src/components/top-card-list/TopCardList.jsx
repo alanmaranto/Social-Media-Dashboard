1090f27c4ba5d00d65f7d9b0c4d1bbb813f793f2
alan <alanarostegui@hotmail.com>
2020-06-07 18:30:08 -0500

    feature(card):  moduling card class

--- src/components/top-card-list/TopCardList.jsx
+++ src/components/top-card-list/TopCardList.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "../card/Card";
+import cardsData from "../../mock/socialMediaData";
 import "./style.css";
 
 const TopCardList = () => {
@@ -6,65 +8,18 @@ const TopCardList = () => {
     <section className="top-cards">
       <div className="wrapper">
         <div className="grid">
-          <article className="card facebook">
-            <p className="card-title">
-              <img src="images/icon-facebook.svg" alt="" />
-              @AlanMaranto
-            </p>
-            <p className="card-followers">
-              <span className="card-followers-number">1487</span>
-              <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-              <img src="images/icon-up.svg" alt="" />
-              12 Today
-            </p>
-          </article>
-
-          <article className="card twitter">
-            <p className="card-title">
-              <img src="images/icon-twitter.svg" alt="" />
-              @Alan-Maranto
-            </p>
-            <p className="card-followers">
-              <span className="card-followers number">185k</span>
-              <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-              <img src="images/icon-up.svg" alt="" />
-              12 today
-            </p>
-          </article>
-
-          <article className="card instagram">
-            <p className="card-title">
-              <img src="images/icon-instagram.svg" alt="" />
-              @Alan-Maranto
-            </p>
-            <p className="card-followers">
-              <span className="card-followers number">185k</span>
-              <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-              <img src="images/icon-up.svg" alt="" />
-              12 today
-            </p>
-          </article>
-
-          <article className="card youtube">
-            <p className="card-title">
-              <img src="images/icon-youtube.svg" alt="" />
-              @SoyAlanMaranto
-            </p>
-            <p className="card-followers">
-              <span className="card-followers number">12k</span>
-              <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-              <img src="images/icon-up.svg" alt="" />
-              12 today
-            </p>
-          </article>
+          {cardsData.map(
+            ({ id, username, followers, todayFollower, icon, name }) => (
+              <Card
+                key={id}
+                username={username}
+                followers={followers}
+                todayFollowers={todayFollower}
+                icon={icon}
+                name={name}
+              />
+            )
+          )}
         </div>
       </div>
     </section>
---
+++ src/mock/socialMediaData.js
@@ -0,0 +1,36 @@
+const cardsData = [
+  {
+    id: 1,
+    username: "@AlanMaranto",
+    followers: 1455,
+    todayFollower: 12,
+    icon: "images/icon-facebook.svg",
+    name: "facebook",
+  },
+  {
+    id: 2,
+    username: "@AlanMaranto2",
+    followers: "28k",
+    todayFollower: 34,
+    icon: "images/icon-twitter.svg",
+    name: "twitter",
+  },
+  {
+    id: 3,
+    username: "@AlanMaranto3",
+    followers: "23k",
+    todayFollower: -5,
+    icon: "images/icon-instagram.svg",
+    name: "instagram",
+  },
+  {
+    id: 4,
+    username: "@AlanMaranto4",
+    followers: "12k",
+    todayFollower: 11,
+    icon: "images/icon-youtube.svg",
+    name: "youtube",
+  },
+];
+
+export default cardsData;
